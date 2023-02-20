97c3406609093395de0239b1d6e09c00f5366933
Konstantin Gribko <czarkost@yandex.ru>
2023-02-20 21:43:20 +0300

    Убраны лишние поля кастомной модели пользователя

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,30 +1,17 @@
-from msilib.schema import Class
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from enum import Enum
 
 
 class User(AbstractUser):
 
     class Role(models.TextChoices):
-        USER = 'user'
-        MODERATOR = 'moderator'
-        ADMIN = 'admin'
+        USER = 'user', ('Пользователь')
+        MODERATOR = 'moderator', ('Модератор')
+        ADMIN = 'admin', ('Администратор')
 
-    CHOICES = (
-
-        (Role.USER, 'user'),
-        (Role.MODERATOR, 'moderator'),
-        (Role.ADMIN, 'admin'),
-
+    role = models.CharField(
+        max_length=15,
+        choices=Role.choices,
+        default=Role.USER,
     )
-    username = models.CharField(max_length=200, unique=True)
-    email = models.EmailField(
-        verbose_name='email адрес',
-        max_length=255,
-        unique=True
-    )
-    password = models.CharField(max_length=255, blank=True, null=True)
-    role = models.CharField(max_length=15, choices=CHOICES, default='user')
     bio = models.TextField(blank=True, null=True)
-    USERNAME_FIELD = 'username'
