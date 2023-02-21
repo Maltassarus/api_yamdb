cb789b8d6c1d3b4f3621bdb356cba9bee9c7f14d
Денис Крохин <krokhin.denis@gmail.com>
2023-02-21 22:54:05 +0300

    Добавлены модели Category, Title, Genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,8 +2,65 @@ from django.db import models
 from users.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Ссылка'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField(max_length=1000)
+    category = models.ForeignKey(
+        Category,
+        related_name='categories',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Название жанра'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Ссылка'
+    )
+    title = models.ForeignKey(
+        Title,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Reviews(models.Model):
-    title = models.ForeignKey()
+    title = models.ForeignKey(
+        Title,
+        related_name='titles',
+        on_delete=models.CASCADE,
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
