8605636b026c1489f0d97eabbfe7a0ae7376890b
Денис Крохин <krokhin.denis@gmail.com>
2023-02-22 18:48:52 +0300

    Поправлены все модели, приведены к единому виду

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,72 +4,68 @@ from users.models import User
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=200,
-        unique=True,
-        verbose_name='Название категории'
+        max_length=256,
+        unique=True
     )
     slug = models.SlugField(
-        max_length=200,
-        unique=True,
-        verbose_name='Ссылка'
+        max_length=50,
+        unique=True
     )
 
     def __str__(self):
-        return self.name
-
-
-class Title(models.Model):
-    name = models.CharField(max_length=200)
-    year = models.IntegerField()
-    description = models.TextField(max_length=1000)
-    category = models.ForeignKey(
-        Category,
-        related_name='categories',
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-
-    def __str__(self):
-        return self.name
+        return self.slug
 
 
 class Genre(models.Model):
     name = models.CharField(
-        max_length=200,
-        unique=True,
-        verbose_name='Название жанра'
+        max_length=256,
+        unique=True
     )
     slug = models.SlugField(
-        max_length=200,
-        unique=True,
-        verbose_name='Ссылка'
+        max_length=50,
+        unique=True
     )
-    title = models.ForeignKey(
-        Title,
+
+    def __str__(self):
+        return self.slug
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        unique=True
+    )
+    year = models.IntegerField()
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
         related_name='titles',
         on_delete=models.SET_NULL,
         null=True,
+        blank=True
     )
+    genre = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.name
 
 
-class Reviews(models.Model):
+class Review(models.Model):
     title = models.ForeignKey(
         Title,
-        related_name='titles',
-        on_delete=models.CASCADE,
+        related_name='Reviews',
+        on_delete=models.CASCADE
     )
     author = models.ForeignKey(
         User,
+        related_name='Reviews',
         on_delete=models.CASCADE,
-        null=False,
+        null=False
     )
     score = models.IntegerField()
     text = models.TextField()
     pub_date = models.DateTimeField(
-        auto_now_add=True,
+        auto_now_add=True
     )
 
 
@@ -80,11 +76,11 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
     review = models.ForeignKey(
-        Reviews,
+        Review,
         related_name='comments',
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
     text = models.TextField(max_length=1000)
     pub_date = models.DateTimeField(
-        auto_now_add=True,
+        auto_now_add=True
     )
