f870a218ded1277524629f1c0f11be9bf1ddec5a
Денис Крохин <krokhin.denis@gmail.com>
2023-02-24 16:03:49 +0300

    views для жанров и категорий

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import SignUpViewSet, token
+from .views import (SignUpViewSet, token,
+                    CategoryCreateListDestroyViewSet,
+                    GenreCreateListDestroyViewSet)
 
 app_name = 'api'
 
 router = SimpleRouter()
 router.register('auth/signup', SignUpViewSet)
-
+router.register('categories', CategoryCreateListDestroyViewSet,
+                basename='categories')
+router.register('genres', GenreCreateListDestroyViewSet,
+                basename='genres')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, status, viewsets
+from rest_framework import mixins, status, viewsets, filters
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework_simplejwt.tokens import AccessToken
 from users.models import User
+from reviews.models import Title, Category, Genre
 
 from .permissions import IsAdminOrSuperuser, IsModerator, IsOwner, ReadOnly
-from .serializers import SignUpSerializer, TokenSerializer
+from .serializers import (SignUpSerializer, TokenSerializer,
+                          CategorySerializer, GenreSerializer)
 
 
 class SignUpViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
@@ -55,3 +58,27 @@ def token(request):
 
     jwt = AccessToken.for_user(user)
     return Response({'token': str(jwt)}, status=status.HTTP_200_OK)
+
+
+class CategoryCreateListDestroyViewSet(mixins.CreateModelMixin,
+                                       mixins.ListModelMixin,
+                                       mixins.DestroyModelMixin,
+                                       viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (ReadOnly | IsAdminOrSuperuser,)
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class GenreCreateListDestroyViewSet(mixins.CreateModelMixin,
+                                    mixins.ListModelMixin,
+                                    mixins.DestroyModelMixin,
+                                    viewsets.GenericViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (ReadOnly | IsAdminOrSuperuser,)
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
